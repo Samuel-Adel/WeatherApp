2309cfa217cebe3659f99f0afd141e24a355ad86
Samuel Adel <samueladel1001@gmail.com>
2024-03-19 17:37:51 +0200

    bottom nav bar done

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    id ("kotlin-kapt")
+    id("kotlin-kapt")
 
 }
 
@@ -51,18 +51,21 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    // bottom navigation bar
+    implementation("com.github.qamarelsafadi:CurvedBottomNavigation:0.1.3")
+
     //Retrofit
-    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     //Room
-    implementation ("androidx.room:room-ktx:2.6.1")
-    implementation ("androidx.room:room-runtime:2.6.1")
-    kapt ("androidx.room:room-compiler:2.6.1")
+    implementation("androidx.room:room-ktx:2.6.1")
+    implementation("androidx.room:room-runtime:2.6.1")
+    kapt("androidx.room:room-compiler:2.6.1")
     //Coroutine
-    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
     //ViewModel
-    implementation ("androidx.lifecycle:lifecycle-extensions:2.2.0")
-    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
     // Picasso
     implementation("com.squareup.picasso:picasso:2.8")
 }
--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -2,10 +2,48 @@ package com.example.weatherapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.qamar.curvedbottomnaviagtion.CurvedBottomNavigation
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val bottomNavBar: CurvedBottomNavigation = findViewById(R.id.bottomNavigation)
+        bottomNavBar.add(
+            CurvedBottomNavigation.Model(1, getString(R.string.home), R.drawable.home_icon)
+
+        )
+        bottomNavBar.add(
+            CurvedBottomNavigation.Model(2, getString(R.string.favourite), R.drawable.fav_icon)
+
+        )
+        bottomNavBar.add(
+            CurvedBottomNavigation.Model(3, getString(R.string.alarm), R.drawable.alarm_icon)
+
+        )
+        bottomNavBar.add(
+            CurvedBottomNavigation.Model(4, getString(R.string.settings), R.drawable.setting_icon)
+
+        )
+        bottomNavBar.show(1)
+        bottomNavBar.setOnClickListener {
+            when (it.id) {
+                1 -> {
+                    bottomNavBar.show(1)
+                }
+
+                2 -> {
+                    bottomNavBar.show(2)
+                }
+
+                3 -> {
+                    bottomNavBar.show(3)
+                }
+
+                4 -> {
+                    bottomNavBar.show(4)
+                }
+            }
+        }
     }
 }
